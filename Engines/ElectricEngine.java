c23fd4716dcab0158e04737c5ca29dd936acf864
Eduard <dirol2016@list.ru>
2022-10-13 16:18:26 +0500

    DZ_2

---
+++ Engines/ElectricEngine.java
@@ -0,0 +1,24 @@
+package Engines;
+
+public class ElectricEngine extends AbstractEngine {
+    @Override
+    protected void StartInternal() {
+        Log("Электрический двигатель запущен");
+    }
+
+    @Override
+    protected void StopInternal() {
+        Log("Электрический двигатель остановлен");
+    }
+
+    @Override
+    protected void SetPowerInternal(int level) {
+        Log(String.format("ШИМ двигателя %d%%", level));
+    }
+
+    @Override
+    public boolean Check() {
+        Log("Электрический двигатель исправен");
+        return true;
+    }
+}
